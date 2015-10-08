2b72028559450cb84b0d44542a9a46347463105b
rashevj <rashev@bk.ru>
2015-10-08 18:26:39 +0300

    select depart

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Тест</title>
+ </head>
+ <body> 
+ <form action="/" method="GET">
 <?php
-echo "hello world";
-print_r($_GET);
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=zayavki', root, "");
+    $departments = array();
+    echo "<select name='parent_depart'>";
+    foreach($dbh->query('SELECT * from depart where parent_id = 0') as $row) {
+    	echo "<option ".(($_GET['parent_depart']==$row['id'])?"selected='selected'":"")." value='".$row['id']."'>".$row['department']."</option>";
+    }
+    echo "</select>";
+    if($_GET['parent_depart']){
+	    echo "<select>";
+	    foreach($dbh->query('SELECT * from depart where parent_id = '.$_GET['parent_depart']) as $row) {
+	    	echo "<option value='".$row['id']."'>".$row['department']."</option>";
+	    }
+	    echo "<select>";
+	}
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
 ?>
+<button>refresh</button>
+</form>
+ </body>
+</html>
+
